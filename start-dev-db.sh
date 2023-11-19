b842b0aaf9d1c1e596a39a85dca3ab40c3ffa002
ulf <uivraeus@gmail.com>
2023-11-19 06:25:43 +0000

    Improve PostgreSQL dev-db handling
    Make it easier  to connect to it from other containers

--- start-dev-db.sh
+++ start-dev-db.sh
@@ -10,8 +10,11 @@ fi
 
 if ! docker-compose --file dev-db/docker-compose.yaml up -d
 then
-  echo "❌ Couln't start the PosgreSQL container"
+  echo "❌ Couldn't start the PostgreSQL container"
   exit 1
 fi
 
-echo '✅ PostgreSQL container is running, check the logs by running "docker logs -f dev-db-db-1"'
+ip_address=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' dev-db_postgres_1)
+echo "✅ PostgreSQL container is running"
+echo "   - Connect via \"localhost\" or ${ip_address}, port 5432"
+echo "   - Check the logs by running \"docker logs -f dev-db_postgres_1\""
